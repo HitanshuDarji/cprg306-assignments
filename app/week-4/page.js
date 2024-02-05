00875a16a54b3ef5ef80ce10550b4abd408f96aa
Hitanshu Janakbhai Darji <130683633+HitanshuDarji@users.noreply.github.com>
2024-02-05 12:34:28 -0700

    Small changes

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 const Page = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-green-950 shadow-md">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-green-950 drop-shadow-2xl">
             <h1 className="mb-8 text-3xl font-bold font">Add New Items</h1>
             <NewItem />
         </div>
